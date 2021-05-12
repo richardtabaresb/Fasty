bbac810879b2077eb68c0d92895fea083f19355b
RichardTB <richardtabaresb@gmail.com>
2021-05-11 21:50:13 -0500

    Diseño de datospersonales_view.xml y uso de fuente externa en logo splash.xml

---
+++ app/src/main/java/bezzier/fasty/datospersonales.kt
@@ -0,0 +1,11 @@
+package bezzier.fasty
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class datospersonales : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.datospersonales_view)
+    }
+}
