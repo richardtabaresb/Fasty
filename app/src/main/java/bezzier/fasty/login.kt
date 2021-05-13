93dde9429cebb77bf1ab1cd7e6c42f781d824484
RichardTB <richardtabaresb@gmail.com>
2021-05-12 21:32:42 -0500

    Diseño de login_view.xml

---
+++ app/src/main/java/bezzier/fasty/login.kt
@@ -0,0 +1,11 @@
+package bezzier.fasty
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login_view)
+    }
+}
