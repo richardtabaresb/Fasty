d65a8808f7a16f949b2e796bad8db3065a339ed9
RichardTB <richardtabaresb@gmail.com>
2021-05-13 15:49:49 -0500

    Diseño de @drawable/splash y puesta en marcha

--- app/src/main/java/bezzier/fasty/registro.kt
+++ app/src/main/java/bezzier/fasty/registro.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 
 class registro : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        Thread.sleep(2000)
+        setTheme(R.style.AppTheme)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.registro_view)
     }
